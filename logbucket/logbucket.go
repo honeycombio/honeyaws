27f2b7fca30198c5c3db6b52537abd0cfde5e56f
Nathan LeClaire <nathan@honeycomb.io>
2017-12-12 15:30:54 -0800

    Update imports
    Signed-off-by: Nathan LeClaire <nathan@honeycomb.io>

--- logbucket/logbucket.go
+++ logbucket/logbucket.go
@@ -11,8 +11,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	"github.com/honeycombio/honeyelb/meta"
-	"github.com/honeycombio/honeyelb/state"
+	"github.com/honeycombio/honeycomb-aws/meta"
+	"github.com/honeycombio/honeycomb-aws/state"
 )
 
 const (
