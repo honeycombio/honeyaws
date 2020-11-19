ea6e9772b65fb81fd71a7a637ac9b3d37bd30f6c
Paul Osman <paul@honeycomb.io>
2020-11-18 21:18:03 -0600

    Add tests for edge mode header parsing

--- publisher/publisher_test.go
+++ publisher/publisher_test.go
@@ -92,6 +92,87 @@ func TestParseTraceData(t *testing.T) {
 	}
 }
 
+func TestParseTraceDataEdgeMode(t *testing.T) {
+	testCases := []struct {
+		ev       event.Event
+		expected map[string]interface{}
+	}{
+		{
+			ev: event.Event{
+				Data: map[string]interface{}{
+					"trace_id":                "Root=1-5759e988-bd862e3fe1be46a994272793;Parent=53995c3f42cd8ad8;Self=1-67891234-12456789abcdef012345678;Sampled=1",
+					"request_processing_time": 1.12,
+					"elb":                     "app/foo-alb/1db0c9806095122a",
+					"request_path":            "/down/stream",
+				},
+			},
+			expected: map[string]interface{}{
+				// old fields
+				"request.headers.x-amzn-trace-id": "Root=1-5759e988-bd862e3fe1be46a994272793;Parent=53995c3f42cd8ad8;Self=1-67891234-12456789abcdef012345678;Sampled=1",
+				"request_processing_time":         1.12,
+				"elb":                             "app/foo-alb/1db0c9806095122a",
+				"request_path":                    "/down/stream",
+				"trace.trace_id":                  "1-5759e988-bd862e3fe1be46a994272793",
+				"trace.span_id":                   "1-67891234-12456789abcdef012345678",
+				"duration_ms":                     1.12,
+				"sampled":                         "1",
+				"service_name":                    "app/foo-alb/1db0c9806095122a",
+				"name":                            "/down/stream",
+			},
+		},
+		{
+			ev: event.Event{
+				Data: map[string]interface{}{
+					// trace id contains some garbage - should not crash
+					"trace_id": "Root=1-5759e988-bd862e3fe1be46a994272793;Parent=53995c3f42cd8ad8;Self=1-67891234-12456789abcdef012345678;Sampled=1;shouldnotcrash",
+					// request time is a string, not a float - should not crash
+					"request_processing_time": "1.12",
+					// elb is not a string, should not crash
+					"elb": 1,
+					// request_path is the wrong type - should not crash
+					"request_path": false,
+				},
+			},
+			expected: map[string]interface{}{
+				"request.headers.x-amzn-trace-id": "Root=1-5759e988-bd862e3fe1be46a994272793;Parent=53995c3f42cd8ad8;Self=1-67891234-12456789abcdef012345678;Sampled=1;shouldnotcrash",
+				"request_processing_time":         "1.12",
+				"elb":                             1,
+				"request_path":                    false,
+				"trace.trace_id":                  "1-5759e988-bd862e3fe1be46a994272793",
+				"trace.span_id":                   "1-67891234-12456789abcdef012345678",
+				"sampled":                         "1",
+			},
+		},
+		{
+			ev: event.Event{
+				Data: map[string]interface{}{
+					"trace_id": "Root=1-5759e988-bd862e3fe1be46a994272793;Parent=53995c3f42cd8ad8;Self=1-67891234-12456789abcdef012345678;Sampled=1",
+					// request processing time, request path, and elb missing - should not crash
+				},
+			},
+			expected: map[string]interface{}{
+				// old fields
+				"request.headers.x-amzn-trace-id": "Root=1-5759e988-bd862e3fe1be46a994272793;Parent=53995c3f42cd8ad8;Self=1-67891234-12456789abcdef012345678;Sampled=1",
+				"trace.trace_id":                  "1-5759e988-bd862e3fe1be46a994272793",
+				"trace.span_id":                   "1-67891234-12456789abcdef012345678",
+				"sampled":                         "1",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		addTraceData(&tc.ev, true)
+		if !reflect.DeepEqual(tc.ev.Data, tc.expected) {
+			t.Error("Output did not match expected:")
+			for k, v := range tc.ev.Data {
+				log.Print("actual: ", k, "\t(", reflect.TypeOf(v), ") ", v)
+				log.Print("expected: ", k, "\t(", reflect.TypeOf(tc.expected[k]), ") ", tc.expected[k])
+			}
+			t.Fatal()
+		}
+	}
+}
+
 func TestDropNegativeTimes(t *testing.T) {
 	testCases := []struct {
 		ev       event.Event
