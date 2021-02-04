fb93286e0e425c15ead2b21e0ca6f36cb8ff3ca7
Paul Osman <paul@honeycomb.io>
2021-02-03 21:11:45 -0600

    Update circle config to support semver style version strings (i.e. v1.4.0)

--- pkg-test/test.sh
+++ pkg-test/test.sh
@@ -7,7 +7,7 @@ set -e
 
 if [ "$#" -ne 1 ]; then echo "usage: test.sh <build id>"; exit 1; fi
 
-BUILDID=$1
+BUILDID=`echo $1 | sed 's/v//g'`
 DEB=honeyaws_${BUILDID}_amd64.deb
 RPM=honeyaws-${BUILDID}-1.x86_64.rpm
 DIR=`dirname $0`
