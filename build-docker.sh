3677d1ee7b0ecf70e26fca276a1f56b291fcd341
Mike Goldsmith <goldsmith.mike@gmail.com>
2021-10-13 14:03:57 +0100

    Build and publish multi-arch docker images on tag (#153)
    When publishing to docker, our images should be built and be runnable on multiple architectures. This PR updates our docker image build process to use Google KO to build the image.

---
+++ build-docker.sh
@@ -0,0 +1,28 @@
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+TAGS="latest"
+VERSION="dev"
+if [[ -n ${CIRCLE_TAG:-} ]]; then
+    # trim 'v' prefix if present
+    VERSION=${CIRCLE_TAG#"v"}
+    # append version to image tags
+    TAGS+=",$VERSION"
+fi
+
+unset GOOS
+unset GOARCH
+export KO_DOCKER_REPO=${KO_DOCKER_REPO:-ko.local}
+export GOFLAGS="-ldflags=-X=main.BuildID=$VERSION"
+export SOURCE_DATE_EPOCH=$(date +%s)
+
+# shellcheck disable=SC2086
+for NAME in honeyalb honeycloudfront honeycloudtrail honeyelb;
+do
+  ko publish \
+    --tags "${TAGS}" \
+    --base-import-paths \
+    --platform "linux/amd64,linux/arm64" \
+    ./cmd/$NAME
+done
